efd3ad430c32976867952c930b8b26629307fedc
Daniel Stuhr Petersen <d@stuhrs.dk>
2020-04-20 21:13:02 +0200

    added text file

--- hide-message-in-bmp.py
+++ hide-message-in-bmp.py
@@ -7,25 +7,18 @@ def encode(bmp_in, message, bmp_out):
     with open(bmp_in, "rb") as bmp_in: 
         with open(bmp_out, "wb") as bmp_out:
             with open(message, "rb") as message:
-                for i in range(HEADSIZE):
-                    bmp_out.write(bmp_in.read(1))
-
+                bmp_out.write(bmp_in.read(HEADSIZE))
                 while mb := message.read(1):
                     for bit in range(7,-1,-1):
                         c = bmp_in.read(1)[0] & 0xFE
                         c = (c | ((mb[0] >> bit) & 0x01))
                         bmp_out.write(bytes([c]))
-
                 for i in range(7,-1,-1):
                     c = bmp_in.read(1)[0] & 0xFE
                     bmp_out.write(bytes([c]))
                 bmp_out.write(bmp_in.read())
             
              
-                
-
-
-
 def decode(bmp_in, txt_out):
     with open(bmp_in, "rb") as bmp_in:
         bmp_in.read(HEADSIZE) # skip header
