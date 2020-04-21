bdaf004b7e3f01db653ffa12e86a894c1cff33aa
Daniel Stuhr Petersen <d@stuhrs.dk>
2020-04-21 12:35:02 +0200

    encode method updated

--- hide-message-in-bmp.py
+++ hide-message-in-bmp.py
@@ -1,21 +1,22 @@
 #!/usr/local/bin/python3
+# BMP file structure: https://en.wikipedia.org/wiki/BMP_file_format
 import sys
 
 HEADSIZE = 120 # Header of BMP image should not be longer
 
-def encode(message, bmp_in, bmp_out):
+def encode(txt_in, bmp_in, bmp_out):
     with open(bmp_in, "rb") as bmp_in: 
         with open(bmp_out, "wb") as bmp_out:
-            with open(message, "rb") as message:
-                bmp_out.write(bmp_in.read(HEADSIZE))
-                while mb := message.read(1):
-                    for bit in range(7,-1,-1):
-                        c = bmp_in.read(1)[0] & 0xFE
-                        c = (c | ((mb[0] >> bit) & 0x01))
-                        bmp_out.write(bytes([c]))
-                for byte in bmp_in.read(8):
-                    bmp_out.write(bytes([byte & 0xFE])) # set the LSB to 0 for 8 bytes after the message = the stop-byte
-                bmp_out.write(bmp_in.read())
+            with open(txt_in, "rb") as txt_in:
+                bmp_out.write(bmp_in.read(HEADSIZE)) # write out original header
+                while msg_byte := txt_in.read(1):
+                    for bit in range(7,-1,-1): # split the msg char bits as insert to last bit of 8 pixel bytes
+                        bmp_byte = bmp_in.read(1)[0] & 0xFE # read in bmp byte and clear last bit 
+                        bmp_byte = (bmp_byte | ((msg_byte[0] >> bit) & 0x01))
+                        bmp_out.write(bytes([bmp_byte]))
+                for byte in bmp_in.read(8): # set the LSB to 0 for 8 bytes after the message = the stop-byte
+                    bmp_out.write(bytes([byte & 0xFE])) 
+                bmp_out.write(bmp_in.read()) # write out the rest of the input bmp file.
             
              
 def decode(bmp_in, txt_out):
@@ -23,7 +24,7 @@ def decode(bmp_in, txt_out):
         bmp_in.read(HEADSIZE) # skip header
         with open(txt_out, "wb") as txt_out:
             while char_bytes := bmp_in.read(8): # read in the next 8 bytes from BMP file
-                char = 0x00 # char byte = all bits off
+                char = 0x00 # new char byte = all bits off
                 for byte in char_bytes:
                     char = (char << 1) | (byte & 0x01) # move all bits one pos to the left and add one bit to the right (a new least significant bit)
                 if char == 0x00: # Stop-byte is reached?
